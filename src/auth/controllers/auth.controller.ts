435128f1d2a8f8d40e7267f056fd48a1a66789c8
GMartins <guilherme.martinst.dev@gmail.com>
2023-03-20 13:50:34 -0300

    fix: endpoint do login alterado para público

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -2,12 +2,14 @@ import { Controller, UseGuards, Request, Post, HttpCode, HttpStatus } from "@nes
 import { AuthService } from "../services/auth.service";
 import { LocalAuthGuard } from "../guards/local.auth.guard";
 import { AuthRequest } from "../models/AuthRequest";
+import { IsPublic } from "../decorators/is.public.decorator";
 
 
 @Controller()
 export class AuthController {
     constructor(private authService: AuthService) {}
 
+    @IsPublic()
     @UseGuards(LocalAuthGuard)
     @Post('auth/login')
     @HttpCode(HttpStatus.OK)
